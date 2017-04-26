737ddc8f97b11df21bbdd605f9e13d51528c074f
coolsmos <martin@macbook-pro-de-martin.home>
2017-04-26 21:40:58 +0200

    nouveaux tps Java EE

---
+++ tp2/src/com/sdzee/tp/beans/Commande.java
@@ -0,0 +1,72 @@
+package com.sdzee.tp.beans;
+
+public class Commande {
+	private String date;
+	private double montant;
+	private String modePaiement;
+	private String StatutPaiement;
+	private String modeLivraison;
+	private String StatutLivraison;
+	private Client client;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public String getModePaiement() {
+		return modePaiement;
+	}
+
+	public void setModePaiement(String modePaiement) {
+		this.modePaiement = modePaiement;
+	}
+
+	public String getStatutPaiement() {
+		return StatutPaiement;
+	}
+
+	public void setStatutPaiement(String statutPaiement) {
+		StatutPaiement = statutPaiement;
+	}
+
+	public String getModeLivraison() {
+		return modeLivraison;
+	}
+
+	public void setModeLivraison(String modeLivraison) {
+		this.modeLivraison = modeLivraison;
+	}
+
+	public String getStatutLivraison() {
+		return StatutLivraison;
+	}
+
+	public void setStatutLivraison(String statutLivraison) {
+		StatutLivraison = statutLivraison;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Commande() {
+		super();
+	}
+
+}
